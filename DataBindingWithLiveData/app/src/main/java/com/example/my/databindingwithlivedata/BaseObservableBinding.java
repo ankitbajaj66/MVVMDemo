7a3592222192f0a4a2c8973b41a48a7d6667ab84
ankitbajaj1989 <ankitbajaj1989@gmail.com>
2019-09-04 00:39:55 +0530

    Added classes

---
+++ DataBindingWithLiveData/app/src/main/java/com/example/my/databindingwithlivedata/BaseObservableBinding.java
@@ -0,0 +1,13 @@
+package com.example.my.databindingwithlivedata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseObservableBinding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_observable_binding);
+    }
+}
---
+++ DataBindingWithLiveData/app/src/main/java/com/example/my/databindingwithlivedata/LiveDataBinding.java
@@ -0,0 +1,13 @@
+package com.example.my.databindingwithlivedata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LiveDataBinding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_data_binding);
+    }
+}
